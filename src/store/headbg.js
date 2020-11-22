3b3f95e6e29ef2858d9d36097fb349ba852690e8
YuDGang <liang1304568590@163.com>
2020-11-23 02:02:39 +0800

    随即背景图

--- src/store/headbg.js
+++ src/store/headbg.js
@@ -2,10 +2,12 @@ import headbg01 from './../assets/img/headbg01.jpg'
 import headbg02 from './../assets/img/headbg02.jpg'
 import headbg03 from './../assets/img/headbg03.jpg'
 import headbg04 from './../assets/img/headbg04.jpg'
+import headbg05 from './../assets/img/headbg05.jpg'
 
 export default [
     headbg01,
     headbg02,
     headbg03,
     headbg04,
+    headbg05
 ]
